5d93ad9e36c3a69332b7eb8125edeab02b020d36
Andrei25i <andreiiosif2504@gmaiil.com>
2025-01-21 22:12:04 +0200

    Added pause command
    Added resume command

---
+++ commands/pause.js
@@ -0,0 +1,33 @@
+const { useQueue } = require("discord-player");
+const { EmbedBuilder } = require("discord.js");
+const config = require("../config");
+
+module.exports = {
+    name: "pause",
+    description: "Pauses the current song",
+    voiceChannel: true,
+
+    async execute({interaction}) {
+        const queue = useQueue(interaction.guild);
+        
+        let defaultEmbed = new EmbedBuilder().setColor(config.embeds.color);
+
+        if (!queue?.isPlaying())
+        {
+            defaultEmbed.setDescription("No music is currently playing right now");
+            return interaction.editReply({ embeds: [defaultEmbed]});
+        }
+
+        if (queue.node.isPaused())
+        {
+            defaultEmbed.setDescription("The player is already paused");
+            return interaction.editReply({ embeds: [defaultEmbed]});
+        }
+
+        queue.node.pause();
+
+        defaultEmbed.setDescription("The song is paused");
+        const message = await interaction.editReply({ embeds: [defaultEmbed] });
+        message.react('⏸');
+    }
+}
---
+++ commands/resume.js
@@ -0,0 +1,33 @@
+const { useQueue } = require("discord-player");
+const { EmbedBuilder } = require("discord.js");
+const config = require("../config");
+
+module.exports = {
+    name: "resume",
+    description: "Resumes the current song",
+    voiceChannel: true,
+
+    async execute({interaction}) {
+        const queue = useQueue(interaction.guild);
+
+        let defaultEmbed = new EmbedBuilder().setColor(config.embeds.color);
+
+        if (!queue?.isPlaying())
+        {
+            defaultEmbed.setDescription("No music is currently playing right now");
+            return interaction.editReply({ embeds: [defaultEmbed]});
+        }
+
+        if (!queue.node.isPaused())
+        {
+            defaultEmbed.setDescription("The player is not paused");
+            return interaction.editReply({ embeds: [defaultEmbed]});
+        }
+
+        queue.node.resume();
+        
+        defaultEmbed.setDescription("The song resumed");
+        const message = await interaction.editReply({ embeds: [defaultEmbed] });
+        message.react('▶️');
+    }
+}
